402c7ca62fb3cfad4655bc1c4cad28a2a69d33f6
Punyawut <Punyawut2017@gmail.com>
2026-02-17 11:16:10 +0700

    Add ResetPassword

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Box, BarChart3, TrendingUp, Lock, Loader2 } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { authService } from "../services/authService";
 
 const LoginPage = () => {
@@ -153,12 +153,12 @@ const LoginPage = () => {
                 className="w-full bg-gray-50 border border-gray-100 rounded-2xl px-6 py-4 text-sm font-bold text-gray-900 placeholder-inactive/50 focus:bg-white focus:border-primary/30 transition-all outline-none"
               />
               <div className="text-right mt-3 mr-2">
-                <a
-                  href="#"
+                <Link
+                  to="/reset-password"
                   className="text-[10px] font-bold text-inactive hover:text-primary uppercase tracking-wider transition-all"
                 >
                   Forgot access?
-                </a>
+                </Link>
               </div>
             </div>
 
